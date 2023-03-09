f68725d2ea0ca9b312fcc9b0fc172c033f9c4f68
Christopher Wong <cw30355@gmail.com>
2023-03-09 16:35:31 -0500

    formatting forms

--- src/Login.js
+++ src/Login.js
@@ -33,7 +33,7 @@ function Login({ getCurrentUser, getAllUsers }) {
 
     return (
         <div className="login">
-            <form onSubmit={handleSubmit} className="login-form">
+            {/* <form onSubmit={handleSubmit} className="login-form">
                 <div className="loginInput">
                     <input className="loginInputs" type="text" placeholder="User Name" value={username} onChange={(e) => handleUserChange(e)}></input>
                     <input className="loginInputs" type="password" placeholder="Password" value={password} onChange={(e) => handlePasswordChange(e)}></input>
@@ -42,8 +42,25 @@ function Login({ getCurrentUser, getAllUsers }) {
                         <button className="logInButton" onClick={handleClick} >New User?</button>
                     </div>
                 </div>
-            </form>
+            </form> */}
+
+            
+            <div class="loginContainer">
+                <form onSubmit={handleSubmit}>
+                    <div class="loginInput" >
+                        <input type="text" value={username} onChange={(e) => handleUserChange(e)}></input>
+                        <label>User Name</label>
+                    </div>
+                    <div class="loginInput">
+                        <input type="password" value={password} onChange={(e) => handlePasswordChange(e)}></input>
+                        <label>Password</label>
+                    </div>
+                    <button type="submit" class="loginButton">Login</button>
+                    <button type="button" class="loginButton"onClick={handleClick}>New User?</button>
+                </form>
+            </div>
         </div>
+
     )
 }
 
